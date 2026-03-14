8b341b2444ad847662a21523d96015307b24d6f4
Xan <test@mail.ru>
2026-03-14 16:27:26 +0500

    add link to family tree project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -64,7 +64,7 @@ export const projects: Project[] = [
     description: "In development.",
     role: "Frontend Layout Developer",
     image: "/placeholder.svg",
-    liveUrl: "https://github.com/Onepunchman-Frg/family-tree",
+    liveUrl: "https://family-tree-two-jet.vercel.app/people",
     tags: ["React", "Tailwind CSS", "NextJS", "Responsive"],
   },
 ];
